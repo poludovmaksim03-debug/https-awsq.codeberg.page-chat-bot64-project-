23678ba1a2b20f042d76eccb5077ecd753659903
poludovmaksim03-debug <poludovmaksim03@gmail.com>
2026-05-16 20:31:08 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,26 @@
+const CACHE_NAME = 'offline-site-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  '/style.css',
+  '/script.js',
+  '/README.md'
+];
+
+
 self.addEventListener('install', event => {
-  console.log('installing[Service Worker]', event);
-
   event.waitUntil(
-    caches.open('static')
-    .then(cache => {
-      console.log('[Service Worker] Precaching App Shell');
-      cache.addAll([
-        '/', 
-        '/index.html',
-        '/favicon.ico',
-        '/src/js/app.js',
-        'src/js/chart.js',
-        'src/js/README.md',
-        'https://poludovmaksim03-debug.github.io/https-awsq.codeberg.page-chat-bot64-project-/'
-      ]);
-    }));
-  });
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
 
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => {
-          if (response) {
-            return response;
-          } else {
-            return fetch(event.request)
-              .then(res => {
-                return caches.open('dynamic')
-                  .then(function(cache) {
-                    cache.put(event.request.url, res.clone());
-                    return res;
-                  })
-              });
-          }
-        })
-    );
-  });
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);       
+      })
+  );
+});
