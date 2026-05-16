6296d2191b7311f9782799c8bb243935bbc38ff2
poludovmaksim03-debug <poludovmaksim03@gmail.com>
2026-05-16 18:05:58 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,38 @@
-const CACHE_NAME = 'v1_site_cache';
-const urlsToCache = [
-  '/',
-  '/html.html',
-  '/style.css',
-  '/script.js'
-];
+self.addEventListener('install', event => {
+  console.log('installing[Service Worker]', event);
 
-self.addEventListener('install', event => {event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
+  event.waitUntil(
+    caches.open('static')
+    .then(cache => {
+      console.log('[Service Worker] Precaching App Shell');
+      cache.addAll([
+        '/', 
+        '/index.html',
+        '/favicon.ico',
+        '/src/js/app.js',
+        'src/js/chart.js',
+        'src/js/README.md',
+        'https://poludovmaksim03-debug.github.io/https-awsq.codeberg.page-chat-bot64-project-/'
+      ]);
+    }));
+  });
 
-self.addEventListener('fetch', event => {event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          if (response) {
+            return response;
+          } else {
+            return fetch(event.request)
+              .then(res => {
+                return caches.open('dynamic')
+                  .then(function(cache) {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                  })
+              });
+          }
+        })
+    );
+  });
